40b1cde2f430bee95d2d7af712cb2e9bfde5c0b9
Brian Shoemaker <bmshoemaker@mail.fhsu.edu>
2025-11-28 08:11:35 -0600

    removed embedded css and merged responsive sections so there was no confusion when running the project.

--- algoviz_pro/settings.py
+++ algoviz_pro/settings.py
@@ -109,13 +109,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+## Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Additional locations for static files
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Directory where collectstatic will collect all static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
